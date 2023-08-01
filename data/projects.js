120f5cacafbc148c2841bf7acc14facc8fb9d72d
aburashidakbarov <abdurshd@icloud.com>
2023-08-01 18:29:11 +0900

    updated projects

--- data/projects.js
+++ data/projects.js
@@ -1,13 +1,11 @@
 const items = [
   {
-    year: "2023.04 - 2023.05",
+    year: "2022.12 - Present",
     projects: [
       {
-        title: "Github Search",
-        description:
-        "This app is light github clone in which you can search for github accounts, open up them and see some repos and other parts of their profiles.",
-        url: "https://github.com/abdurshd/github-searcher",
-        icon: "star",
+        title: "Doston-Law",
+        url: "https://github.com/abdurshd/doston-law",
+        description: "Official web page of the 'Doston Law' law firm",
       },
     ],
   },
@@ -18,6 +16,17 @@ const items = [
         title: "House Bazaar",
         url: "https://github.com/abdurshd/house-bazaar",
         description: "Platform for selling/renting houses",
+      },
+    ],
+  },
+  {
+    year: "2023.04 - 2023.05",
+    projects: [
+      {
+        title: "Github Search",
+        description:
+          "This app is light github clone in which you can search for github accounts, open up them and see some repos and other parts of their profiles.",
+        url: "https://github.com/abdurshd/github-searcher",
         icon: "star",
       },
     ],
@@ -32,28 +41,6 @@ const items = [
     ],
   },
   {
-    year: "2023.01 - Present",
-    projects: [
-      {
-        title: "Portfolio",
-        url: "https://github.com/abdurshd/portfolio",
-        description: "Personal portfolio webpage",
-        icon: "star",
-      },
-    ],
-  },
-  {
-    year: "2022.12 - Present",
-    projects: [
-      {
-        title: "Doston-Law",
-        url: "https://github.com/abdurshd/doston-law",
-        description: "Official web page of the 'Doston Law' law firm",
-        icon: "star",
-      },
-    ],
-  },
-  {
     year: "2022.11",
     projects: [
       {
@@ -62,6 +49,18 @@ const items = [
       },
     ],
   },
-  ];
+  {
+    year: "2013",
+    projects: [
+      {
+        title: "Gomatch",
+        url: "https://github.com/abdurshd/to-do-List",
+        description:
+          "Match any number of patterns/events in single line log records.",
+        icon: "star",
+      },
+    ],
+  },
+];
 
 export default items;
