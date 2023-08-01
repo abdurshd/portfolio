6490f8a6d5a4ebf825429f2c854c982c89ac95d2
aburashidakbarov <abdurshd@icloud.com>
2023-08-01 18:23:11 +0900

    changed projects

--- data/projects.js
+++ data/projects.js
@@ -1,99 +1,67 @@
 const items = [
   {
-    year: "2022",
+    year: "2023.04 - 2023.05",
     projects: [
       {
-        title: "Tmux Colours: VSCode",
-        url: "https://github.com/halafi/tmux-colours-vscode",
-        description: "A tmux colour theme that looks like vscode.",
-      },
-    ],
-  },
-  {
-    year: "2021",
-    projects: [
-      {
-        title: "test.vim",
-        url: "https://github.com/vim-test/vim-test",
-        description: "Run your tests at the speed of thought",
-        icon: "star",
-        stats: "2.3k+ stars",
-      },
-    ],
-  },
-  {
-    year: "2020",
-    projects: [
-      {
-        title: "Elder.js",
+        title: "Github Search",
         description:
-          "Opinionated static site generator and web framework for Svelte",
-        url: "https://github.com/Elderjs/elderjs",
-        icon: "star",
-        stats: "2.1k+ stars",
-      },
-      {
-        title: "Minesweeper",
-        description: "PWA built into Android APK (Chrome 72 powered)",
-        url: "https://github.com/halafi/minesweeper",
-        icon: "flag-2",
-      },
-    ],
-  },
-  {
-    year: "2019",
-    projects: [
-      {
-        title: "Cookie Consent",
-        url: "https://github.com/osano/cookieconsent",
-      },
-      {
-        title: "quickact",
-        url: "https://github.com/halafi/quickact",
-      },
-    ],
-  },
-  {
-    year: "2018",
-    projects: [
-      {
-        title: "react-dates-lite",
-        url: "https://github.com/dominikchrastek/react-dates-lite",
-      },
-    ],
-  },
-  {
-    year: "2017",
-    projects: [
-      {
-        title: "dotfiles",
-        description: "My shell, vim and tmux config files. Updated regularly.",
-        url: "https://github.com/halafi/dotfiles",
+        "This app is light github clone in which you can search for github accounts, open up them and see some repos and other parts of their profiles.",
+        url: "https://github.com/abdurshd/github-searcher",
         icon: "star",
       },
     ],
   },
   {
-    year: "2015",
+    year: "2023.03 - Present",
     projects: [
       {
-        title: "Apache Spark benchmark",
-        url: "https://github.com/securitycloud/spark",
-      },
-    ],
-  },
-  {
-    year: "2013",
-    projects: [
-      {
-        title: "Gomatch",
-        url: "https://github.com/halafi/gomatch",
-        description:
-          "Match any number of patterns/events in single line log records.",
+        title: "House Bazaar",
+        url: "https://github.com/abdurshd/house-bazaar",
+        description: "Platform for selling/renting houses",
         icon: "star",
       },
     ],
   },
-];
+  {
+    year: "2023.01",
+    projects: [
+      {
+        title: "Comment App",
+        url: "https://github.com/abdurshd/commentApp",
+      },
+    ],
+  },
+  {
+    year: "2023.01 - Present",
+    projects: [
+      {
+        title: "Portfolio",
+        url: "https://github.com/abdurshd/portfolio",
+        description: "Personal portfolio webpage",
+        icon: "star",
+      },
+    ],
+  },
+  {
+    year: "2022.12 - Present",
+    projects: [
+      {
+        title: "Doston-Law",
+        url: "https://github.com/abdurshd/doston-law",
+        description: "Official web page of the 'Doston Law' law firm",
+        icon: "star",
+      },
+    ],
+  },
+  {
+    year: "2022.11",
+    projects: [
+      {
+        title: "To Do List",
+        url: "https://github.com/abdurshd/to-do-List",
+      },
+    ],
+  },
+  ];
 
 export default items;
