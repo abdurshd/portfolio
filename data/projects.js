1a24dc4c680b6fa8f001b825e150eab833f95b6c
aburashidakbarov <kakaabdurashid1995@gmail.com>
2023-09-03 23:43:49 +0900

    added extra explanation for doston-law

--- data/projects.js
+++ data/projects.js
@@ -63,10 +63,10 @@ const items = [
     projects: [
       {
         title: "Doston-Law",
-        url: "https://github.com/abdurshd/doston-law",
+        url: "https://doston-law.uz",
         description: "Official web page of the 'Doston Law' law firm",
         icon: "star",
-        overallExpl: 'It is a Next.js full stack web app for a law firm in Uzbekistan',
+        overallExpl: 'It is a Next.js full stack web app for a law firm in Uzbekistan. For the protection of APIs, github source code is private for this website',
         usedSkillsExpl: 'For this project Next.JS, Next SEO, Kbar, React Rough Notation, framer-motion and other small tools have been used.',
         image: 'https://i.ibb.co/wJcZF9M/Screenshot-2023-08-03-at-6-34-28-PM.png',
         deployedLink: 'https://doston-law.uz'
